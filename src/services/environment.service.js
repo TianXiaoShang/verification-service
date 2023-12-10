65a5d28d64c2eb7c470c06be1940cdc46d3f2698
johnny <qlu@knx.com.cn>
2023-12-11 02:42:28 +0800

    feature: 修改正式环境

--- src/services/environment.service.js
+++ src/services/environment.service.js
@@ -1,7 +1,8 @@
-const BASE_URL = 'https://6test.djlnet.com.cn/app/appointment_api.php?r='
+// const BASE_URL = 'https://6test.djlnet.com.cn/app/appointment_api.php?r='
+const BASE_URL = 'https://movie.djlcom.net/app/appointment_api.php?r='
 
-const NEW_BASE_URL = 'https://6test.djlnet.com.cn/commission/api/1'
-// const NEW_BASE_URL = 'https://movie.djlcom.net/commission/api/1'
+// const NEW_BASE_URL = 'https://6test.djlnet.com.cn/commission/api/1'
+const NEW_BASE_URL = 'https://movie.djlcom.net/commission/api/1'
 
 
 export { BASE_URL, NEW_BASE_URL }
